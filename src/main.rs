cc5a3dd4bcdefc5c386e4a409856a13fce4b6a96
Ozel <ozelcode@gmail.com>
2022-03-03 21:20:38 -0600

    delete comment

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std] 
+#![no_main]
 
 
-/// This function is called on panic.
+
 use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
